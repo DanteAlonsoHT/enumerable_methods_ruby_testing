14e17739327af784a205ee6c98591bf00c3a0b27
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-14 11:19:38 -0500

    Add my_each test

--- spec/testing.rb
+++ spec/testing.rb
@@ -5,3 +5,17 @@ hash_test = { 'key1' => 1, 'key2' => 2 }
 range_test = (1..5)
 symbol_test = [:sym1, :sym2]
 path_test = ["document", "memes", "doge"]
+
+describe Enumerable do
+    describe '#my_each' do
+        it "returns the same class when no block was given" do
+            expect(array_test.my_each.class).to eql array_test.each.class  
+        end
+        it "returns when array was given" do
+            expect(array_test.my_each do |i| end).to eql (array_test.each do |i| end)
+        end
+        it "returns when hash was given" do
+            expect(hash_test.my_each do |key, value| end).to eql (hash_test.each do |key, value| end) 
+        end
+    end
+end
