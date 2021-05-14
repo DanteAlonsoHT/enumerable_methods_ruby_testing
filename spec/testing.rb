3f76ddc86914c022c0c4b81b4ee04a1b8821447b
DanteAlonsoHT <dante1818@hotmail.com>
2021-05-14 11:18:18 -0500

    Add Path of main file as require_relative

---
+++ spec/testing.rb
@@ -0,0 +1,7 @@
+require_relative '../enumerable_methods_ruby'
+
+array_test = [2,0,2,1]
+hash_test = { 'key1' => 1, 'key2' => 2 }
+range_test = (1..5)
+symbol_test = [:sym1, :sym2]
+path_test = ["document", "memes", "doge"]
